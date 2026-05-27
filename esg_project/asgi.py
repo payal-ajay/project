15ad7a248ef6a0b12042f6dbe70d9f962b096ff1
payal-ajay <payalajay2005@yahoo.com>
2026-05-28 00:34:08 +0530

    Fix: change breathe module references to esg_project

--- esg_project/asgi.py
+++ esg_project/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'breathe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esg_project.settings')
 
 application = get_asgi_application()
--- esg_project/wsgi.py
+++ esg_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'breathe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esg_project.settings')
 
 application = get_wsgi_application()
